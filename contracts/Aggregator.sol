38915ee79f1857d83c526372f2a320ca62d4706d
zorancuc <cuc.zoran@gmail.com>
2021-03-03 16:30:07 +0100

    udpate variables

--- contracts/Aggregator.sol
+++ contracts/Aggregator.sol
@@ -12,13 +12,13 @@ contract Aggregator {
     uint public constant PRICE_START = 0.01 ether;
     uint public constant PRICE_END = 0.05 ether;
     uint public constant OFFSET = 100;
-    address public constant WETH = 0xd0a1e359811322d97991e03f863a0c30c2cf029c;
+    address public constant WETH = 0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1;
     uint public constant DEADLINE = 100 days;
 
     uint public blockNumberStart;
     uint public blockNumberEnd;
     address public owner;
-    uint public QTotalSold;
+    uint public totalAmountSold;
     bool public isStarted;
     address public greedAddr;
     address public orderAddr;
@@ -39,7 +39,7 @@ contract Aggregator {
     function start() external {
         require(msg.sender == owner, "ASC: PERMISSION DENIED");
         blockNumberStart = block.number;
-        QTotalSold = 0;
+        totalAmountSold = 0;
         isStarted = true;
         orderId = 0;
     }
@@ -49,41 +49,41 @@ contract Aggregator {
         require(isStarted, "ASC: NOT STARTED");
         require(blockNumberCurrent <= blockNumberStart.add(DURATION), "ASC: PERIOD IS OVER");
         require(strike >= PRICE_END, "ASC: Strike Price IS LOW");
-        uint priceBound = PRICE_START.add((PRICE_END.sub(PRICE_START)).mul(blockNumberCurrent.sub(blockNumberStart)).div(DURATION));
-        uint Qest = msg.value.div(priceBound);
-        uint k = coeff(Qest);
-        uint Qsold = Qest.mul(k).div(100);
-        QTotalSold = QTotalSold.add(Qsold);
+        uint priceLowerBound = PRICE_START.add((PRICE_END.sub(PRICE_START)).mul(blockNumberCurrent.sub(blockNumberStart)).div(DURATION));
+        uint estimatedAmount = msg.value.div(priceLowerBound);
+        uint bonusMultiplier = calculateBonusMultiplier(estimatedAmount);
+        uint amountSold = estimatedAmount.mul(bonusMultiplier).div(100);
+        totalAmountSold = totalAmountSold.add(amountSold);
 
         //Create event for Order NFT
-        IOrder(orderAddr).createOrder(msg.sender, greedAddr, WETH, Qsold, Qsold.mul(strike), msg.sender, blockNumberStart.add(orderId).add(DEADLINE), 0, 0, 0);
-        emit OrderCreated(msg.sender, orderId, now, strike, Qsold);
+        IOrder(orderAddr).createOrder(msg.sender, greedAddr, WETH, amountSold, amountSold.mul(strike), msg.sender, blockNumberStart.add(orderId).add(DEADLINE), 0, 0, 0);
+        emit OrderCreated(msg.sender, orderId, now, strike, amountSold);
         orderId = orderId.add(1);
     }
 
-    function coeff(uint Qest) internal returns(uint k) {
-        uint val = Qest.mul(100).div(QTotalSold.add(OFFSET));
+    function calculateBonusMultiplier(uint estimatedAmount) internal returns(uint bonusMultiplier) {
+        uint val = estimatedAmount.mul(100).div(totalAmountSold.add(OFFSET));
         if (val >= 500) {
-            k = 150;
+            bonusMultiplier = 150;
         } else if (val >= 200) {
-            k = 140;
+            bonusMultiplier = 140;
         } else if (val >= 100) {
-            k = 130;
+            bonusMultiplier = 130;
         } else if (val >= 50) {
-            k = 120;
+            bonusMultiplier = 120;
         } else if (val >= 25) {
-            k = 115;
+            bonusMultiplier = 115;
         } else if (val >= 10) {
-            k = 110;
+            bonusMultiplier = 110;
         } else if (val >= 5) {
-            k = 105;
+            bonusMultiplier = 105;
         } else if (val >= 1) {
-            k = 101;
+            bonusMultiplier = 101;
         }
     }
 
     function end() external {
         require (block.number >= blockNumberStart.add(DURATION), "ASC: PERIOD IS NOT OVER");
-        IGreed(greedAddr).mint(to, QTotalSold.mul(2));
+        IGreed(greedAddr).mint(to, totalAmountSold.mul(2));
     }
 }
